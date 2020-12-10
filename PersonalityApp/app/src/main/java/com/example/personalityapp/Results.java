f5d5d119013322bfa39dee8c7bfa6db214d453bb
Hana <pareceh@mymail.vcu.edu>
2020-12-09 20:57:13 -0500

    CodeStyle was mad at me

--- PersonalityApp/app/src/main/java/com/example/personalityapp/Results.java
+++ PersonalityApp/app/src/main/java/com/example/personalityapp/Results.java
@@ -1,6 +1,5 @@
 package com.example.personalityapp;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
--- PersonalityApp/app/src/main/java/com/example/personalityapp/TypeE.java
+++ PersonalityApp/app/src/main/java/com/example/personalityapp/TypeE.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TypeE extends AppCompatActivity {
